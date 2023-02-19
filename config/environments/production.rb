a3bc3a4623e0a10ddb967616d41f654202466131
Jorge Luis <jluis.developer@gmail.com>
2023-02-19 18:56:33 -0500

    configure enviroments mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV["SMTP_SERVER_ADDRESS"],
+    port:                 ENV["SMTP_SERVER_PORT"],
+    domain:               ENV["SMTP_SERVER_DOMAIN"],
+    user_name:            ENV["SMTP_ACCOUNT_ADDRESS"],
+    password:             ENV["SMTP_ACCOUNT_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
